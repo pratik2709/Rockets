ed59d1756652d7ff50ed77b373133b53610c5760
PRATIK SAXENA <pratik2709@gmail.com>
2015-06-21 15:32:40 +0530

    Refactored key code files

--- js/rocket.js
+++ js/rocket.js
@@ -1,50 +1,5 @@
-var runner = (function(run){
-    run.rocket = (function(){
-// The keycodes that will be mapped when a user presses a button.
-// Original code by Doug McInnes
-KEY_CODES = {
-  32: 'space',
-  37: 'left',
-  38: 'up',
-  39: 'right',
-  40: 'down'
-};
-// Creates the array to hold the KEY_CODES and sets all their values
-// to false. Checking true/flase is the quickest way to check status
-// of a key press and which one was pressed when determining
-// when to move and which direction.
-KEY_STATUS = {};
-for (var code in KEY_CODES) {
-  KEY_STATUS[ KEY_CODES[ code ]] = false;
-}
-/**
- * Sets up the document to listen to onkeydown events (fired when
- * any key on the keyboard is pressed down). When a key is pressed,
- * it sets the appropriate direction to true to let us know which
- * key it was.
- */
-document.onkeydown = function(e) {
-  // Firefox and opera use charCode instead of keyCode to
-  // return which key was pressed.
-  var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
-  if (KEY_CODES[keyCode]) {
-    e.preventDefault();
-    KEY_STATUS[KEY_CODES[keyCode]] = true;
-  }
-};
-/**
- * Sets up the document to listen to ownkeyup events (fired when
- * any key on the keyboard is released). When a key is released,
- * it sets teh appropriate direction to false to let us know which
- * key it was.
- */
-document.onkeyup = function(e) {
-  var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
-  if (KEY_CODES[keyCode]) {
-    e.preventDefault();
-    KEY_STATUS[KEY_CODES[keyCode]] = false;
-  }
-};
+var runner = (function (run) {
+    run.rocket = (function () {
 
         //move the rocket
         // setting up the rocket
@@ -52,7 +7,7 @@ document.onkeyup = function(e) {
         //of individual sprite
         // can also be done the other way around
         //by specifying the number of frames explicitly
-        this.x =64;
+        this.x = 64;
         this.y = 50;
         this.width = 256;
         this.height = 256;
@@ -65,8 +20,8 @@ document.onkeyup = function(e) {
         this.anim = new run.Animation(this.sheet, 4, 0, 4);
 
         //move
-        var that =this;
-        this.move = function(){
+        var that = this;
+        this.move = function () {
             //this.x =64;
             //this.y = 50;
             //this.speed = 6;
@@ -75,18 +30,18 @@ document.onkeyup = function(e) {
             //    run.initial.ctx.clearRect(64,that.y,that.width, that.height);
             //}
 
-            if(KEY_STATUS.up){
+            if (KEY_STATUS.up) {
                 that.y -= that.speed;
 
-                if(that.y <= -(run.initial.canvas.height*0.5)){
+                if (that.y <= -(run.initial.canvas.height * 0.5)) {
                     that.y = 0;
                 }
             }
-            if(KEY_STATUS.down) {
+            if (KEY_STATUS.down) {
                 that.y += that.speed;
 
                 if (that.y >= run.initial.canvas.height) {
-                    that.y = run.initial.canvas.height*0.5;
+                    that.y = run.initial.canvas.height * 0.5;
                 }
             }
 
