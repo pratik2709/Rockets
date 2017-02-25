8df725c508d118726e884c3442d4df025e833f54
Pratik Saxena <pratik2709@gmail.com>
2017-02-25 20:57:15 +0530

    PS: refactored asset loader

--- js/assetloader.js
+++ js/assetloader.js
@@ -1,69 +1,64 @@
 // For loading all assets
 
-var runner = (function (run) {
-
-    run.assetLoader = (function () {
+var assetLoader = (function () {
 
 
-        this.images = {
-            'bg': 'imgs/bg.png',
-            'sky': 'imgs/sky.png',
-            'backdrop': 'imgs/backdrop.png',
-            'backdrop2': 'imgs/backdrop_ground.png',
-            'grass': 'imgs/grass.png',
-            'avatar_normal': 'imgs/normal_walk.png',
-            'rocket': 'imgs/rocket2.png',
-            'bullet': 'imgs/bullet.png'
-        };
+    this.images = {
+        'bg': 'imgs/bg.png',
+        'sky': 'imgs/sky.png',
+        'backdrop': 'imgs/backdrop.png',
+        'backdrop2': 'imgs/backdrop_ground.png',
+        'grass': 'imgs/grass.png',
+        'avatar_normal': 'imgs/normal_walk.png',
+        'rocket': 'imgs/rocket2.png',
+        'bullet': 'imgs/bullet.png'
+    };
 
-        var assets_loaded = 0;
-        var number_of_images = Object.keys(this.images).length;
+    var assets_loaded = 0;
+    var number_of_images = Object.keys(this.images).length;
 
 
-        this.totalAssets = number_of_images;
+    this.totalAssets = number_of_images;
 
-        function assetLoaded(dic, name) {
+    function assetLoaded(dic, name) {
 
-            if (this[dic][name].status !== 'loading') {
-                return;
-            }
-            this[dic][name].status = "loaded";
-            assets_loaded++;
-            if (assets_loaded === number_of_images && typeof this.finished === "function") {
-                this.finished();
+        if (this[dic][name].status !== 'loading') {
+            return;
+        }
+        this[dic][name].status = "loaded";
+        assets_loaded++;
+        if (assets_loaded === number_of_images && typeof this.finished === "function") {
+            this.finished();
+        }
+    }
+
+    this.downloadAllAssets = function () {
+        var that = this;
+        var source;
+        //load images
+        for (var image in this.images) {
+
+            if (this.images.hasOwnProperty(image)) {
+                source = this.images[image];
+                (function (that, image) {
+                    that.images[image] = new Image();
+                    that.images[image].status = "loading";
+                    that.images[image].name = image;
+                    that.images[image].onload = function () {
+                        //wierd passing string
+                        assetLoaded.call(that, "images", image)
+                    };
+                    that.images[image].src = source
+
+                })(that, image);
+
             }
         }
+    };
 
-        this.downloadAllAssets = function () {
-            var that = this;
-            var source;
-            //load images
-            for (var image in this.images) {
-
-                if (this.images.hasOwnProperty(image)) {
-                    source = this.images[image];
-                    (function (that, image) {
-                        that.images[image] = new Image();
-                        that.images[image].status = "loading";
-                        that.images[image].name = image;
-                        that.images[image].onload = function () {
-                            //wierd passing string
-                            assetLoaded.call(that, "images", image)
-                        };
-                        that.images[image].src = source
-
-                    })(that, image);
-
-                }
-            }
-        };
-
-        return {
-            images: this.images,
-            totalAssets: this.totalAssets,
-            downloadAllAssets: this.downloadAllAssets
-            //finished: this.finshed
-        }
-    })();
-    return run;
-})(runner || {});
+    return {
+        images: this.images,
+        totalAssets: this.totalAssets,
+        downloadAllAssets: this.downloadAllAssets
+    }
+})();
--- js/main.js
+++ js/main.js
@@ -1,85 +1,67 @@
-var runner = (function (run) {
-
-    // initialize the canvas
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    var player = {};
-    var ground = [];
-    var platformWidth = 32;
-    var platformHeight = canvas.height - platformWidth * 4;
-
-    var ballRad = 50;
-    var posX = 500;
-    var posY = 300;
-    var ball = new run.ball(ballRad * ballRad, ballRad, new Vector2(posX, posY), new Vector2(0, 0));
-
-    //var rocket = run.rocket.init();
-    //console.log(run.rocket);
-
-
-    var requestAnimFrame = (function () {
-        return window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function (callback, element) {
-                window.setTimeout(callback, 1000 / 60);
-            };
-    })();
-
-    function animate() {
-        requestAnimFrame(animate);
-        ctx.clearRect(0, 0, run.initial.canvas.width, run.initial.canvas.height);
-
-        run.background.draw();
-        ball.draw(ctx, "#ff0000");
-        run.rocket.animate.update();
-        // decide what will be the values of x and y input depending on the keyboard input
-        run.rocket.move();
-        run.rocket.bulletPool.animate();
-        //console.log(run.rocket.bulletPool);
-        //run.rocket.animate.draw(64, 50);
-        var contacts = collide();
-        //console.log(contacts);
-
-    }
-
-    function startGame() {
-
-        //console.log(run.rocket.animate);
-        run.initial.myObjects.push(ball);
-        //mObjects.push(run.rocket.bulletPool);
-
-        //ground tiles?
-        //initialize the background
-        run.background.reset();
-        animate();
-
-    }
-
-    function collide() {
-        var contacts = [];
-
-        for (var ii = 0; ii < run.initial.myObjects.length - 1; ii++) {
-            for (var jj = ii + 1; jj < run.initial.myObjects.length; jj++) {
-                if (run.initial.myObjects[ii].mass != 0 || run.initial.myObjects[jj].mass != 0) {
-                    var _contacts = run.initial.myObjects[ii].getClosestPoints(run.initial.myObjects[jj]);
-                    contacts = contacts.concat(_contacts);
-                }
-            }
-        }
-
-        return contacts;
-    }
-
-    run.assetLoader.finished = function () {
-        startGame();
-    };
-
-    run.assetLoader.downloadAllAssets();
-
-})(runner || {});
+//var canvas = document.getElementById("canvas");
+//var ctx = canvas.getContext("2d");
+//var platformWidth = 32;
+//var platformHeight = canvas.height - platformWidth * 4;
+//
+//var ballRad = 50;
+//var posX = 500;
+//var posY = 300;
+//var ball = new run.ball(ballRad * ballRad, ballRad, new Vector2(posX, posY), new Vector2(0, 0));
+//
+//
+//function animate() {
+//    requestAnimFrame(animate);
+//    ctx.clearRect(0, 0, run.initial.canvas.width, run.initial.canvas.height);
+//
+//    run.background.draw();
+//    ball.draw(ctx, "#ff0000");
+//    run.rocket.animate.update();
+//    run.rocket.move();
+//    run.rocket.bulletPool.animate();
+//    var contacts = collide();
+//
+//}
+//
+//function startGame() {
+//
+//    run.initial.myObjects.push(ball);
+//    run.background.reset();
+//    animate();
+//
+//}
+//
+//function collide() {
+//    var contacts = [];
+//
+//    for (var ii = 0; ii < run.initial.myObjects.length - 1; ii++) {
+//        for (var jj = ii + 1; jj < run.initial.myObjects.length; jj++) {
+//            if (run.initial.myObjects[ii].mass != 0 || run.initial.myObjects[jj].mass != 0) {
+//                var _contacts = run.initial.myObjects[ii].getClosestPoints(run.initial.myObjects[jj]);
+//                contacts = contacts.concat(_contacts);
+//            }
+//        }
+//    }
+//
+//    return contacts;
+//}
 
 
+assetLoader.finished = function () {
+    console.log("checking");
+    startGame();
+};
+
+assetLoader.downloadAllAssets();
+
+
+var requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback, element) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
 
